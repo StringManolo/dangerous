e99bab625b59b08a16f7cdd7bb38c33fc41d9917
StringManolo <manuelvarelacaldas@gmail.com>
2020-11-07 01:19:56 +0100

    Implementing Myszkowski Transposition Cipher

---
+++ Myszkowski.py
@@ -0,0 +1,92 @@
+def Myszkowski(string, key):
+# alfabetical position number :
+    keyOrder = []
+    i = 0
+    while i < len(key):
+        keyOrder.append(ord(key[i])-96)
+        i += 1
+
+#    print(keyOrder)
+    
+
+# bubble sorting :
+    keyOcurr = keyOrder[:]
+    i = 0
+    while i < len(keyOrder):
+        j = 0
+        while j < (len(keyOrder) - 1):
+            if keyOrder[j] > keyOrder[j+1]:
+                keyOrder[j], keyOrder[j+1] = keyOrder[j+1], keyOrder[j]
+            j += 1
+        i += 1
+
+#    print(keyOrder)
+
+
+# find first ocurrence and change to char :
+ #    print(f"""
+ #
+ #keyOrder:
+ #{keyOrder}
+ #
+ #keyOcurr:
+ #{keyOcurr}
+ #
+ #""")
+    i = 0
+    j = 0
+    k = 97
+    already = []
+    while i < len(keyOrder):
+        j = 0
+        while j < (len(keyOrder) ):
+#            print(f"Comparing keyOrder[i] ({keyOrder[i]}) and keyOcurr[j] ({keyOcurr[j]})")
+            if keyOrder[i] == keyOcurr[j]:
+#                print(f"\n\nKeyOrder ({keyOrder[i]}) found in position {i}")
+#                print(f"Replacing ({keyOcurr[j]}) by {chr(k)}")
+                l = 0
+                while l < len(already):
+#                    print(f"Testing if char(k) ({chr(k)}) is equal to already[l] ({already[l]})")
+                    if keyOrder[i] == already[l]:
+#                        print(f"This char ({chr(k)}) was already found.")
+                        k -= 1
+                        break
+                    l += 1
+            
+                already.append(keyOrder[i])
+                keyOcurr[j] = chr(k)
+                k += 1
+            j += 1
+        i += 1
+
+#    print(keyOcurr)
+   
+    i = 0
+    while i < len(keyOcurr):
+        keyOcurr[i] = ord(keyOcurr[i]) - 96
+        i += 1
+
+    print(f"""[{', '.join(map(str, list(key)))}]
+{keyOcurr}
+""")
+
+
+
+    i = 0
+    j = 0
+    table = []
+    row = []
+    while i < len(string):
+        if j == len(key):
+            table.append(row)
+            j = 0
+            row = []
+        row.append(string[i])
+        i += 1
+        j += 1
+
+    prettyPrint = ']\n['.join(map(lambda x: ', '.join(map(str, x)), table))
+    print(f"[{prettyPrint}]")
+
+Myszkowski("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789", "swindon")
+#Myszkowski("", "abxdcazbeafgbmhai")
